49085c14d2e2bd9e128e2d91a85994b8adfe91c5
KamalDGRT <kamaldgrt@gmail.com>
2021-12-19 23:23:32 +0530

    vscode: add bat file to install extensions

---
+++ Windows/Microsoft Visual Studio Code/Install_VS_Code_Extensions.bat
@@ -0,0 +1,39 @@
+@ECHO OFF
+ECHO Installing Extensions for VS Code
+
+code --install-extension 2gua.rainbow-brackets
+code --install-extension oderwat.indent-rainbow
+code --install-extension BeardedBear.beardedicons
+code --install-extension dracula-theme.theme-dracula
+code --install-extension ElAnandKumar.el-vsc-product-icon-theme
+
+code --install-extension mechatroner.rainbow-csv
+code --install-extension mikestead.dotenv
+code --install-extension vincaslt.highlight-matching-tag
+
+code --install-extension esbenp.prettier-vscode
+code --install-extension foxundermoon.shell-format
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension silvenon.mdx
+
+code --install-extension qwtel.sqlite-viewer
+code --install-extension tomoki1207.pdf
+
+code --install-extension ambar.bundle-size
+code --install-extension dsznajder.es7-react-js-snippets
+code --install-extension eg2.vscode-npm-script
+code --install-extension herrmannplatz.npm-dependency-links
+code --install-extension WallabyJs.quokka-vscode
+
+code --install-extension svelte.svelte-vscode
+code --install-extension ardenivanov.svelte-intellisense
+code --install-extension fivethree.vscode-svelte-snippets
+
+code --install-extension bmewburn.vscode-intelephense-client
+
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-toolsai.jupyter
+
+code --install-extension ms-vscode.cpptools
+PAUSE
